4b45ebfe787af82867cef1ed5b9540a162a19350
Michael McEwingE <michamcewing@gmail.com>
2024-02-06 01:12:34 -0800

    finish

---
+++ app.py
@@ -0,0 +1,45 @@
+
+import requests
+from flask import Flask, request, render_template
+
+
+# Build a Single Page Flask Application that has a home page that just greets the user, and 
+# then a second route that will have a form to enter a pokemon name that will on submit update the page with 
+# some properties listed about the pokemon(properties listed below).  Use your homework (Week 4: Day 4) from 
+# when we did this during python to help you with your API call. 
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<p>Welcome!</p>"
+
+def fetchPokeInfo(pokeName):
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokeName}"
+    response = requests.get(url)
+    if response.ok:
+        data = response.json()
+        abilityUrl = data['abilities'][0]['ability']['url']
+        abilityResponse = requests.get(abilityUrl)
+        abilityData = abilityResponse.json()
+        return [data, abilityData]
+    else:
+        return "An Error has occured. Please try again..."
+
+
+
+@app.route("/poke-info")
+def getPokeInfo():
+    return render_template("pokeSearch.html")
+
+
+@app.route("/poke-info", methods=["POST"])
+def addPokeInfo():
+    name = request.form.get("name")
+    info = fetchPokeInfo(name)
+    print(info)
+    return render_template("pokeSearch.html", info=info[0], abilityInfo=info[1])
+
+        
