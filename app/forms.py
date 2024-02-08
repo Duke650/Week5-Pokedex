8040d16f8d2b5f26ff2464cecc8bd5636ce4292c
Michael McEwingE <michamcewing@gmail.com>
2024-02-08 00:49:00 -0800

    forms added

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField, PasswordField
 from wtforms.validators import DataRequired
 
 class SearchPokemon(FlaskForm):
     name = StringField('name: ', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+class Login(FlaskForm):
+    email = EmailField("Email", validators=[DataRequired()])
+    password = PasswordField("Password: ", validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class Signup(FlaskForm):
+    username = StringField('Username: ', validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
+    password = PasswordField("Password: ", validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,9 @@
 import requests
 from flask import Flask, request, render_template
 from app import app
-from .forms import SearchPokemon
+from .forms import SearchPokemon, Login, Signup
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+
 
 def fetchPokeInfo(pokeName):
     url = f"https://pokeapi.co/api/v2/pokemon/{pokeName}"
@@ -20,7 +18,19 @@ def fetchPokeInfo(pokeName):
     else:
         return None, "Oops! Seems this Pokemon is not in your Pokedex. Please try again..."
 
-
+def fetchIdOfPokemon(name):
+    url = f"https://pokeapi.co/api/v2/pokemon/{name}"
+    response = requests.get(url)
+    if response.ok:
+        data = response.json()
+        id = data['id']
+        print("id =>", id)
+        return id
+    
+    # ---- ROUTES -----
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 @app.route("/poke-info", methods=["GET","POST"])
 def getPokeInfo():
@@ -33,4 +43,27 @@ def getPokeInfo():
         return render_template("pokeSearch.html", form=form, defaultInfo=defaultInfo, abilityInfo=abilityInfo)
     return render_template("pokeSearch.html", form=form)
 
-    
+@app.route("/poke-info/<id>", methods=["GET","POST"])
+def getPokeInfoById(id):
+    form = SearchPokemon()
+    if form.validate_on_submit() and request.method == "POST":
+        name = request.form.get("name")
+        info = fetchPokeInfo(name)
+        defaultInfo = info[0]
+        abilityInfo = info[1]
+        return render_template("pokeSearch.html", form=form, defaultInfo=defaultInfo, abilityInfo=abilityInfo)
+    return render_template("pokeSearch.html", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    loginForm = Login()
+    if loginForm.validate_on_submit() and request.method == "POST":
+        return render_template("login.html", loginForm=loginForm)
+    return render_template("login.html", loginForm=loginForm)
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    signupForm = Signup()
+    if signupForm.validate_on_submit() and request.method == "POST":
+        return render_template("signup.html", signupForm=signupForm)
+    return render_template("signup.html", signupForm=signupForm)
